64d59d5730277cc823f43dfc9891816f812cc3bd
Pete Gautier <pete@momentohq.com>
2022-10-27 13:39:29 -0700

    feat: using strict types

--- src/Auth/ICredentialProvider.php
+++ src/Auth/ICredentialProvider.php
@@ -1,9 +1,13 @@
 <?php
+declare(strict_types=1);
+
 namespace Momento\Auth;
 
 interface ICredentialProvider
 {
-    public function getAuthToken() : string;
-    public function getControlEndpoint() : string;
+    public function getAuthToken(): string;
+
+    public function getControlEndpoint(): string;
+
     public function getCacheEndpoint() : string;
 }
--- src/Cache/_ControlGrpcManager.php
+++ src/Cache/_ControlGrpcManager.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Momento\Cache;
 
@@ -7,21 +8,22 @@ use Grpc\ChannelCredentials;
 use Control_client\ScsControlClient;
 
 
-class _ControlGrpcManager {
+class _ControlGrpcManager
+{
 
     public ScsControlClient $client;
 
     public function __construct(string $authToken, string $endpoint)
     {
         $options = [
-            "update_metadata" => function($metadata) use ($authToken) {
+            "update_metadata" => function ($metadata) use ($authToken) {
                 $metadata["authorization"] = [$authToken];
                 $metadata["agent"] = ["php:0.1"];
                 return $metadata;
             }
         ];
 
-        $channel = new Channel($endpoint, ["credentials"=>ChannelCredentials::createSsl()]);
+        $channel = new Channel($endpoint, ["credentials" => ChannelCredentials::createSsl()]);
         $this->client = new ScsControlClient($endpoint, $options, $channel);
     }
 
