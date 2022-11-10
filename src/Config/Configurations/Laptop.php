b3f357caeb1f7cef99ac5983643d86025ea14f09
Pete Gautier <pete@momentohq.com>
2022-11-10 15:15:33 -0800

    fix configuration preset organization

--- src/Config/Configurations/Laptop.php
+++ src/Config/Configurations/Laptop.php
@@ -6,13 +6,15 @@ namespace Momento\Config\Configurations;
 use Momento\Config\Configuration;
 use Momento\Config\Transport\StaticGrpcConfiguration;
 use Momento\Config\Transport\StaticTransportStrategy;
-use Monolog\Logger;
+use Momento\Utilities\LoggingHelper;
+use Psr\Log\LoggerInterface;
 
 class Laptop extends Configuration
 {
 
-    public static function latest(?Logger $logger = null): Laptop
+    public static function latest(?LoggerInterface $logger = null): Laptop
     {
+        $logger = $logger ?? LoggingHelper::getNullLogger("null");
         $grpcConfig = new StaticGrpcConfiguration(5000);
         $transportStrategy = new StaticTransportStrategy(null, $grpcConfig, $logger);
         return new Laptop($logger, $transportStrategy);
