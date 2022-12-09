4144285e6463d7667ce3673ec90a6efc3937a8b4
Pete Gautier <pete@momentohq.com>
2022-12-09 13:45:13 -0800

    chore: add benchmark/loadgen scripts and docs

---
+++ examples/index-loadgen.php
@@ -0,0 +1,61 @@
+<?php
+ini_set('display_errors', 1);
+require 'vendor/autoload.php';
+
+use Momento\Auth\EnvMomentoTokenProvider;
+use Momento\Cache\SimpleCacheClient;
+use Momento\Config\Configurations\Laptop;
+
+$cacheName = "php-loadgen";
+$authProvider = new EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN");
+$config = Laptop::latest();
+$defaultTtl = 600;
+
+$gets = [];
+$sets = [];
+
+$startTime = -hrtime(true);
+$client = new SimpleCacheClient($config, $authProvider, $defaultTtl);
+$startup = $startTime + hrtime(true);
+
+$errors = 0;
+$misses = 0;
+$value = str_repeat("x", 100);
+for ($i = 0; $i < 5; $i++) {
+    $setStart = -hrtime(true);
+    $response = $client->set($cacheName, "myKey", $value);
+    if ($err = $response->asError()) {
+        $errors++;
+        continue;
+    } else {
+        $setTime = $setStart + hrtime(true);
+    }
+
+    $getStart = -hrtime(true);
+    $response = $client->get($cacheName, "myKey");
+    if ($err = $response->asError()) {
+        $errors++;
+        continue;
+    } elseif ($response->asMiss()) {
+        $misses++;
+        continue;
+    } else {
+        $getTime = $getStart + hrtime(true);
+    }
+    $gets[] = $getTime;
+    $sets[] = $setTime;
+}
+
+$startupFile = fopen("startups.txt", "a");
+$getsFile = fopen("gets.txt", "a");
+$setsFile = fopen("sets.txt", "a");
+fwrite($startupFile, "$startup\n");
+fwrite($getsFile, join("\n", $gets) . "\n");
+fwrite($setsFile, join("\n", $sets) . "\n");
+fclose($startupFile);
+fclose($getsFile);
+fclose($setsFile);
+
+$endTime = $startTime + hrtime(true);
+$endSecs = $endTime / 1e+9;
+print "misses: $misses, errors: $errors in $endTime nanoseconds ($endSecs secs)\n";
---
+++ examples/loadgen.php
@@ -0,0 +1,106 @@
+<?php
+declare(strict_types=1);
+
+require "vendor/autoload.php";
+
+use Momento\Auth\EnvMomentoTokenProvider;
+use Momento\Cache\SimpleCacheClient;
+use Momento\Config\Configurations\Laptop;
+
+class LoadGenerator
+{
+    private const CACHE_NAME = "php-loadgen";
+    private int $totalNumberOfOperationsToExecute;
+    private string $cacheValue;
+    private int $startTime;
+    public array $getNanoseconds = [];
+    public array $setNanoseconds = [];
+    public int $errors = 0;
+
+    public function __construct(
+        int $cacheItemPayloadBytes,
+        int $totalNumberOfOperationsToExecute
+    )
+    {
+        $this->totalNumberOfOperationsToExecute = $totalNumberOfOperationsToExecute;
+        $this->cacheValue = str_repeat("x", $cacheItemPayloadBytes);
+    }
+
+    public function nanosecondsToSeconds(int $nanoseconds): float
+    {
+        return $nanoseconds / 1e+9;
+    }
+
+    private function setUp(): void
+    {
+        $client = $this->getClient();
+        $response = $client->createCache(self::CACHE_NAME);
+        if ($error = $response->asError()) {
+            throw $error->innerException();
+        }
+        $this->startTime = -hrtime(true);
+    }
+
+    private function getClient(): SimpleCacheClient
+    {
+        $cache_item_ttl_seconds = 60;
+        $authProvider = new EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN");
+        $configuration = Laptop::latest();
+        return new SimpleCacheClient(
+            $configuration, $authProvider, $cache_item_ttl_seconds
+        );
+    }
+
+    public function run(): void
+    {
+        $this->setUp();
+        for ($i = 1; $i <= $this->totalNumberOfOperationsToExecute; $i++) {
+            $client = $client ?? $this->getClient();
+            if ($i % 1000 == 0) {
+                print "$i operations in " . $this->nanosecondsToSeconds($this->startTime + hrtime(true)) . " seconds.\n";
+            }
+
+            $cache_key = "single-worker-operation-$i";
+
+            // execute and time a set
+            $hrtime = -hrtime(true);
+            $response = $client->set(self::CACHE_NAME, $cache_key, $this->cacheValue);
+            if ($response->asSuccess()) {
+                $setHrtime = $hrtime + hrtime(true);
+            } else {
+                print "Set error: $response\n";
+                $this->errors++;
+                continue;
+            }
+
+            // execute and time a get
+            $hrtime = -hrtime(true);
+            $response = $client->get(self::CACHE_NAME, $cache_key);
+            if ($response->asHit()) {
+                $getHrtime = $hrtime + hrtime(true);
+            } else {
+                print "Get error: $response\n";
+                $this->errors++;
+                continue;
+            }
+            $this->setNanoseconds[] = $setHrtime;
+            $this->getNanoseconds[] = $getHrtime;
+        }
+    }
+}
+
+$loadGenerator = new LoadGenerator(100, 100000);
+$loadGenerator->run();
+$gets = fopen("gets.txt", "w");
+while ($nextGet = array_shift($loadGenerator->getNanoseconds)) {
+    fwrite($gets, "$nextGet\n");
+}
+fclose($gets);
+
+$sets = fopen("sets.txt", "w");
+while ($nextSet = array_shift($loadGenerator->setNanoseconds)) {
+    fwrite($sets, "$nextSet\n");
+}
+fclose($sets);
+
+print "Completed with {$loadGenerator->errors} errors.\n";
