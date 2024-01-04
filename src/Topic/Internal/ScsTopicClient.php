292f9420d3393153cb1af45136dfbb9c16c4aca7
rishtigupta <rishti@momentohq.com>
2024-01-03 16:46:28 -0800

    fix: example run

--- src/Topic/Internal/ScsTopicClient.php
+++ src/Topic/Internal/ScsTopicClient.php
@@ -166,7 +166,7 @@ class ScsTopicClient implements LoggerAwareInterface
                     foreach ($call->responses() as $response) {
                         try {
                             $this->logger->info("Received message from topic $topicName in cache $cacheName\n");
-                            $this->logger->info("Received message content: " . json_encode($response));
+                            $this->logger->info("Received message content: $response\n");
                             $onMessage($response);
                         } catch (\Exception $e) {
                             $this->logger->error("Error processing message: " . $e->getMessage());
